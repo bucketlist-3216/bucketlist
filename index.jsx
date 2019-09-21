a0ec1d04631c0dcddddf0aeac02a02c4e1aeae58
Renyi Tan <tanrenyi@hotmail.com>
2019-09-21 14:19:22 -0400

    setup react router

--- index.jsx
+++ index.jsx
@@ -1,6 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './src/client/App';
-import './index.scss'
+import './index.scss';
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#root')
+);
--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
 
 import LoginView from './routes/LoginView';
-import './styles.scss'
+import './styles.scss';
 
 const App = () => {
+  // return <LoginView />;
   return (
-    <LoginView />
-  );
+    <Switch>
+      <Route exact path='/' component={LoginView} />
+    </Switch>
+  )
 };
 
-export default App
+export default App;
