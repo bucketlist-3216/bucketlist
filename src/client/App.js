427875befae98088cceab924f228299093498e5a
Renyi Tan <tanrenyi@hotmail.com>
2019-09-21 12:52:49 -0400

    enable scss config with webpack

--- src/client/App.js
+++ src/client/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button'
-import './styles.css'
+import './styles.scss'
 
 const App = () => {
   return (
-    <div>
-      <div>Hello World</div>
+    <div className="test">
+      <div className="text">Hello World</div>
       <Button />
     </div>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -19,8 +19,8 @@ module.exports = {
         ]
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
       }
     ]
   },
