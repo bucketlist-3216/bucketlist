4ec62ec08d3f8e7900044fff88eb5326349a48c4
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-09-24 12:39:09 +0800

    add facebook sign in front end functionality

--- src/client/components/SingleSignOnButton/SingleSignOnButton.jsx
+++ src/client/components/SingleSignOnButton/SingleSignOnButton.jsx
@@ -4,8 +4,13 @@ import PropTypes from 'prop-types';
 import { Button } from 'react-bootstrap';
 
 import GoogleLogin from 'react-google-login';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props'
 
 const responseGoogle = (response) => {
+  console.log(response);
+}
+
+const responseFacebook = (response) => {
   console.log(response);
 }
 
@@ -14,32 +19,39 @@ const SingleSignOnButton = props => {
   const identity = providerName.toLowerCase();
 
   if (identity === 'google') {
-      return (
-          <GoogleLogin
-            clientId="INSERT_CLIENT_ID.apps.googleusercontent.com"
-            render={renderProps => (
-              <div>
-                <button className="sso-button" onClick={renderProps.onClick} disabled={renderProps.disabled}>
-                  {logo}
-                  <span>{providerName}</span>
-                </button>
-              </div>
-            )}
-            buttonText={providerName}
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
-          />
-      );
-  } else {
-      return (
-        <div>
-          <button className="sso-button">
-            {logo}
-            <span>{providerName}</span>
-          </button>
-        </div>
-      );
+    return (
+      <GoogleLogin
+        clientId="INSERT_CLIENT_ID.apps.googleusercontent.com"
+        render={renderProps => (
+          <div>
+            <button className="sso-button" onClick={renderProps.onClick} disabled={renderProps.disabled}>
+              {logo}
+              <span>{providerName}</span>
+            </button>
+          </div>
+        )}
+        buttonText={providerName}
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={'single_host_origin'}
+      />
+    );
+  } else if (identity === 'facebook') {
+    return (
+      <FacebookLogin
+        appId="INSERT_APP_ID"
+        autoLoad
+        callback={responseFacebook}
+        render={renderProps => (
+          <div>
+            <button className="sso-button" onClick={renderProps.onClick}>
+              {logo}
+              <span>{providerName}</span>
+            </button>
+          </div>
+        )}
+      />
+    );
   }
 };
 
