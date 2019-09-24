3063cdb101a87a8f53566dd7e61e4f55024ba390
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-09-24 12:20:05 +0800

    add google sign in front end functionality

--- src/client/components/SingleSignOnButton/SingleSignOnButton.jsx
+++ src/client/components/SingleSignOnButton/SingleSignOnButton.jsx
@@ -3,18 +3,44 @@ import PropTypes from 'prop-types';
 
 import { Button } from 'react-bootstrap';
 
+import GoogleLogin from 'react-google-login';
+
+const responseGoogle = (response) => {
+  console.log(response);
+}
+
 const SingleSignOnButton = props => {
   const { domain, providerName, logo } = props;
   const identity = providerName.toLowerCase();
 
-  return (
-    <div>
-      <button className="sso-button">
-        {logo}
-        <span>{providerName}</span>
-      </button>
-    </div>
-  );
+  if (identity === 'google') {
+      return (
+          <GoogleLogin
+            clientId="INSERT_CLIENT_ID.apps.googleusercontent.com"
+            render={renderProps => (
+              <div>
+                <button className="sso-button" onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                  {logo}
+                  <span>{providerName}</span>
+                </button>
+              </div>
+            )}
+            buttonText={providerName}
+            onSuccess={responseGoogle}
+            onFailure={responseGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
+      );
+  } else {
+      return (
+        <div>
+          <button className="sso-button">
+            {logo}
+            <span>{providerName}</span>
+          </button>
+        </div>
+      );
+  }
 };
 
 SingleSignOnButton.propTypes = {
