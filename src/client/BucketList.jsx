83fdb6bc14765cbb7ce69f16c2f3fe05136005b6
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2019-09-25 21:38:14 +0530

    Integrate Google Analytics (#16)
    * add google analytics code
    
    * Splash page + Cleanup  (#19)
    
    * SplashPage done
    
    * rename styles/layouts -> styles/pages
    
    * cleanup CreateView and add autoBindMethods decorator
    
    * Add login functionality (#15)
    
    * add frontend login response handler
    
    * add backend login functionality
    
    * refactor logic from SSOButtons -> Login component
    
    * update package-lock.json
    
    * remove <p>
    
    * Set up GA trackers
    
    * update handle name
    
    * uncomment ga tracker in landing page
    
    * Add list page (#21)
    
    * edit voting results api
    
    * edit list UI
    
    * include image_link in voting results
    
    * add buttons on list page
    
    * make button redirect to respective pages
    
    * edit swipe link
    
    * Fix Trips backend functionalities and improve links between pages (#22)
    
    * fix list page layout for desktop
    
    * edit get user trip api endpoint
    
    * move user trips api to constants
    
    * edit links between pages and abstract the frontend paths into constants folder
    
    * resize create page components
    
    * static assets bundled in production build (#18)

--- src/client/BucketList.jsx
+++ src/client/BucketList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom';
+import ReactGA from 'react-ga';
+
 import './styles/styles.scss';
 
 import PATHS from './constants/paths';
@@ -10,6 +12,25 @@ import LandingPage from './pages/LandingPage';
 import SwipeView from './pages/SwipeView';
 import TripsPage from './pages/TripsPage';
 import ListPage from './pages/ListPage';
+
+ReactGA.initialize('UA-148749594-1');
+ReactGA.pageview(window.location.pathname + window.location.search)
+
+// const BucketList = () => {
+//   return (
+//     <Switch>
+//       <Route exact path='/' component={LandingPage} />
+//     </Switch>
+//   )
+// };
+
+// export default BucketList;
+
+
+// function initializeReactGA() {
+//   ReactGA.initialize('UA-148749594-1');
+//   ReactGA.pageview('/homepage');
+// }
 
 ReactDOM.render(
   <BrowserRouter>
--- src/client/pages/SwipeView/SwipeView.jsx
+++ src/client/pages/SwipeView/SwipeView.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import ReactGA from 'react-ga';
+
 import Header from "../../components/Header";
 import Swipe from "../../components/Swipe";
 
@@ -8,6 +10,9 @@ class SwipeView extends Component {
   }
 
   render() {
+    ReactGA.initialize('UA-148749594-1');
+    ReactGA.pageview(window.location.pathname + window.location.search);
+    
     return (
       <div>
         <Header />
