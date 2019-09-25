f5c8475105ec25f88a1dd3c41011cc447ca5367b
Renyi Tan <tanrenyi@hotmail.com>
2019-09-24 21:31:33 -0400

    rename styles/layouts -> styles/pages

--- src/client/BucketList.jsx
+++ src/client/BucketList.jsx
@@ -7,13 +7,11 @@ import CreateView from './pages/CreateView';
 import LandingPage from './pages/LandingPage';
 import SwipeView from './pages/SwipeView';
 import TripsPage from './pages/TripsPage';
-import SplashPage from './pages/SplashPage';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      {/* <Route exact path="/" component={LandingPage} /> */}
-      <Route exact path="/" component={SplashPage} />
+      <Route exact path="/" component={LandingPage} />
       <Route path='/createview' component={CreateView} />
       <Route path='/swipeview' component={SwipeView} />
       <Route path='/user/:userId/trips' component={TripsPage} />
