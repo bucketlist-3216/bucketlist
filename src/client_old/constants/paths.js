caf2e1821313ef2919e309ffd489784c4c41a231
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-10-13 19:44:12 +0800

    Revert "Archive Assignment 3 UI  (#53)" (#57)
    This reverts commit fdcbcc9691efae9c6c49ff7aa3d75d2ef5ee52e7.

--- src/client_old/constants/paths.js
+++
@@ -1,9 +0,0 @@
-const PATHS = {
-  landingPage: '/',
-  createTrip: (userId = ':userId') => `/user/${userId}/create-trip`,
-  swipe: (userId = ':userId', tripId = ':tripId') => `/user/${userId}/trip/${tripId}/swipe`,
-  trips: (userId = ':userId') => `/user/${userId}/trips`,
-  list: (userId = ':userId', tripId = ':tripId') => `/user/${userId}/trip/${tripId}/list`
-};
-
-export default PATHS;
