ac9bc552ef18566750ef07fbb9d097d505fc5157
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2019-10-14 23:53:48 +0800

    update to percentage in scss

--- src/client/BucketList.jsx
+++ src/client/BucketList.jsx
@@ -32,6 +32,7 @@ ReactDOM.render(
       <Route exact path={PATHS.createTrip()} component={CreateTrip} />
       <Route exact path={PATHS.citySelect()} component={CitySelect} />
       <Route exact path={PATHS.swipe()} component={SwipeView} />
+      <Route exact path={PATHS.list()} component={ListPage} />
       {/* <Route exact path={PATHS.landingPage} component={AppHome} /> */}
       {/* <Route path={PATHS.createTrip()} component={CreateView} />
       <Route path={PATHS.swipe()} component={SwipeView} /> */}
