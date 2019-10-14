ee196760e7c71b125c57f87f51b8bc3b74cb76a6
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2019-10-15 02:43:49 +0800

    List page api call (#78)
    * list page api call
    
    * title case Singapore

--- src/client/constants/paths.js
+++ src/client/constants/paths.js
@@ -5,7 +5,7 @@ const PATHS = {
   trips: () => '/trips',
   createTripOld: (userId = ':userId') => `/user/${userId}/create-trip`,
   swipe: (tripId = `:tripId`) => `/swipe/${tripId}`,
-  list: (tripId = ':tripId') => `/list/${tripId}`,
+  list: (tripId = `:tripId`) => `/list/${tripId}`,
   swipeOld: (userId = ':userId', tripId = ':tripId') => `/user/${userId}/trip/${tripId}/swipe`,
   tripsOld: (userId = ':userId') => `/user/${userId}/trips`,
   listOld: (userId = ':userId', tripId = ':tripId') => `/user/${userId}/trip/${tripId}/list`
