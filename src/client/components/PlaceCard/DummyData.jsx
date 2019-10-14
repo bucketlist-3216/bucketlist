f2b07eb8744b240dd64397dd1d3e8b35a32a5469
Ang Zhi Yuan <angzy95@yahoo.com.sg>
2019-10-14 11:34:11 +0800

    Cherry Pick commit SHA 728ee0a5c9064d68c3634451264e9184a3b0244a

--- src/client/components/PlaceCard/DummyData.jsx
+++ src/client/components/PlaceCard/DummyData.jsx
@@ -1,12 +1,54 @@
-const DummyPlace = {
-  place_id: 1,
-  name: "Ya Kun Kaya Toast",
-  address: "1 Fusionopolis Way, #B1-15, S'138632",
-  image_link: null,
-  voteCount: {
-    LIKE: 5,
-    DISLIKE: 0
+const DummyPlaces = [
+  {
+    place_id: 1,
+    name: "Ya Kun Kaya Toast, $",
+    address: "1 Fusionopolis Way, #B1-15, S'138632",
+    image_link: null,
+    voteCount: {
+      LIKE: 5,
+      DISLIKE: 0
+    }
+  },
+  {
+    place_id: 2,
+    name: "Gardens By The Bay, $$",
+    address: "18 Marina Gardens Dr, S'018953",
+    image_link: null,
+    voteCount: {
+      LIKE: 4,
+      DISLIKE: 1
+    }
+  },
+  {
+    place_id: 3,
+    name: "Marina Bay Sands, $$$",
+    address: "10 Bayfront Ave, S'018956",
+    image_link: null,
+    voteCount: {
+      LIKE: 3,
+      DISLIKE: 2
+    }
+  },
+  {
+    place_id: 4,
+    name: "Lau Pa Sat Satay, $",
+    address: "18 Raffles Quay, S'048582",
+    image_link: null,
+    voteCount: {
+      LIKE: 2,
+      DISLIKE: 3
+    }
+  },
+  {
+    place_id: 5,
+    name: "Singapore Flyer, $$",
+    address: "30 Raffles Ave, S'039803",
+    image_link: null,
+    voteCount: {
+      LIKE: 2,
+      DISLIKE: 3
+    }
   }
-};
+];
 
-export default DummyPlace;
+export default DummyPlaces;
