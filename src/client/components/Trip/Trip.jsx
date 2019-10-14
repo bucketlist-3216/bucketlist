f166553b85711b956244d4000d974fee2295f87d
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-10-15 02:17:16 +0800

    fix redirecting from trips to places and some css bugs (#77)
    * fix redirect to places from trips
    
    * fix css bugs

--- src/client/components/Trip/Trip.jsx
+++ src/client/components/Trip/Trip.jsx
@@ -5,26 +5,20 @@ import { faUserAlt, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import PATHS from '../../constants/paths';
 
 const Trip = props => {
-  const { trip, userId, showModal } = props;
+  const { trip, userId, showModal, onClick } = props;
   let { trip_id, destination, start_date, end_date, members } = trip;
   if (!members) {
     members = [];
   }
 
-  function routeChange(pathname) {
-    props.history.push({
-      pathname
-    });
-  }
-
-  function decodePromisified() { 
+  function decodePromisified() {
     return new Promise(function(resolve, reject) {
-      jwt.verify(token, secret, resolve) 
-    }) 
+      jwt.verify(token, secret, resolve)
+    })
   }
 
   return (
-    <div className="trip">
+    <div className="trip" onClick={onClick}>
       <div className="row">
         <div className="trip-element">
           <img src={`../../../../assets/trips/${destination}.png`} alt={destination} className="trip-img"/>
