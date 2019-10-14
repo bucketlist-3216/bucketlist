785546d74766b2af5308de6c300f56aa6920cf94
Ang Zhi Yuan <angzy95@yahoo.com.sg>
2019-10-14 13:55:14 +0800

    Add demo pictures

--- src/client/components/PlaceCard/DummyData.jsx
+++ src/client/components/PlaceCard/DummyData.jsx
@@ -3,7 +3,7 @@ const DummyPlaces = [
     place_id: 1,
     name: "Ya Kun Kaya Toast, $",
     address: "1 Fusionopolis Way, #B1-15, S'138632",
-    image_link: null,
+    image_link: "../../../../assets/demo/YaKunKayaToast.jpg",
     voteCount: {
       LIKE: 5,
       DISLIKE: 0
@@ -13,7 +13,7 @@ const DummyPlaces = [
     place_id: 2,
     name: "Gardens By The Bay, $$",
     address: "18 Marina Gardens Dr, S'018953",
-    image_link: null,
+    image_link: "../../../../assets/demo/GardensByTheBay.jpg",
     voteCount: {
       LIKE: 4,
       DISLIKE: 1
@@ -23,7 +23,7 @@ const DummyPlaces = [
     place_id: 3,
     name: "Marina Bay Sands, $$$",
     address: "10 Bayfront Ave, S'018956",
-    image_link: null,
+    image_link: "../../../../assets/demo/MarinaBaySands.jpg",
     voteCount: {
       LIKE: 3,
       DISLIKE: 2
@@ -33,7 +33,7 @@ const DummyPlaces = [
     place_id: 4,
     name: "Lau Pa Sat Satay, $",
     address: "18 Raffles Quay, S'048582",
-    image_link: null,
+    image_link: "../../../../assets/demo/LauPaSatSatay.jpg",
     voteCount: {
       LIKE: 2,
       DISLIKE: 3
@@ -43,7 +43,7 @@ const DummyPlaces = [
     place_id: 5,
     name: "Singapore Flyer, $$",
     address: "30 Raffles Ave, S'039803",
-    image_link: null,
+    image_link: "../../../../assets/demo/SingaporeFlyer.jpg",
     voteCount: {
       LIKE: 2,
       DISLIKE: 3
