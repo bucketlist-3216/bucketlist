7cab15ac3812f0e4ce132e156b59dc7bed06084e
Ang Zhi Yuan <angzy95@yahoo.com.sg>
2019-10-28 16:46:35 +0800

    Edit InvitePage show Google Login natively
    InvitePage previously redirected to LoginPage if not logged in, but this allows the user to login as Guest.
    This workflow forces the user to login using Google to be invited.

--- src/client/pages/InvitePage/InvitePage.jsx
+++ src/client/pages/InvitePage/InvitePage.jsx
@@ -3,14 +3,21 @@ import ReactGA from 'react-ga';
 import autoBindMethods from 'class-autobind-decorator';
 
 import PATHS from '../../constants/paths';
+import PROVIDERS from '../../constants/providers';
 import APIS from '../../constants/apis';
 import axios from "axios";
 
+import SingleSignOnButton from '../../components/SingleSignOnButton';
 
 @autoBindMethods
 class InvitePage extends Component {
   constructor(props) {
     super (props);
+
+    this.state = {
+      isLoading: false,
+      loggedIn: false
+    };
   }
 
   routeChange(pathname, state) {
@@ -19,13 +26,23 @@ class InvitePage extends Component {
     });
   }
 
+  setLoggedIn() {
+    this.setState({loggedIn: true});
+  }
+
+  setLoading(isLoading) {
+    this.setState({ isLoading });
+  }
+
+  redirectToTrip(tripIdJson) {
+    this.routeChange(PATHS.list(tripIdJson.data.tripId));
+  }
+
   render() {
     ReactGA.initialize('UA-148749594-1');
     ReactGA.pageview(window.location.pathname + window.location.search);
 
-    if (!localStorage.getItem('token')) {
-      this.routeChange(PATHS.login, {"returnPrevious": true});
-    } else {
+    if (localStorage.getItem('platform')  && localStorage.getItem('platform') !== 'jwt') {
       const { inviteLink } = this.props.match.params
       console.log("inviteLink: ", inviteLink);
       axios.request({
@@ -35,7 +52,21 @@ class InvitePage extends Component {
           token: localStorage.getItem('token'),
           platform: localStorage.getItem('platform')
         }
-      }).then((tripIdJson) => this.routeChange(PATHS.list(tripIdJson.tripId)));
+      }).then(this.redirectToTrip);
+    } else {
+      return (
+        <div className="app-home">
+          <h1 className="title">bucketlist</h1>
+          <div className="row">
+            <SingleSignOnButton
+              providerName={PROVIDERS['google'].providerName}
+              logo={PROVIDERS['google'].logo}
+              setLoading={this.setLoading}
+              onLoginSuccess={() => this.setLoggedIn}
+            />
+          </div>
+        </div>
+      );
     }
     return null;
   }
