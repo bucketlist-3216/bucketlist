1152d854a2d92a6e54f06abcfd8fd1996e8f2212
unknown <tanyonghe@u.nus.edu>
2019-11-03 16:02:19 +0800

    Add image numbers so users know how many images there are for the current location card

--- src/client/components/Swipe/Swipe.jsx
+++ src/client/components/Swipe/Swipe.jsx
@@ -253,7 +253,7 @@ class Swipe extends Component {
         >
           <SwipeCard place={currentPlace} setPlaceData={this.setPlaceData} setShowInfo={this.setShowInfo} 
             imageIndex={imageIndex} imageChange={this.imageChange} setInitialScreenX={this.setInitialScreenX} 
-            renderResult={renderResult} />
+            renderResult={renderResult} imageIndex={imageIndex} numOfImgs={this.state.places[0].images.length} />
           <InfoPanel place={currentPlace} showInfo={showInfo} setShowInfo={this.setShowInfo}/>
         </Swipeable>
         {places.length > 1 && <SwipeCard zIndex={-1} place={places[1]} imageIndex={0} />}
