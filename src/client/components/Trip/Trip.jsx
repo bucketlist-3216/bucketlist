9c7ed9ab260b0235d717044f950328b70658cdd2
Karen Frilya Celine <karenfrilya97@gmail.com>
2019-11-10 04:12:01 +0800

    show trip_name, dates and members in profile page

--- src/client/components/Trip/Trip.jsx
+++ src/client/components/Trip/Trip.jsx
@@ -6,10 +6,12 @@ import PATHS from '../../constants/paths';
 
 const Trip = props => {
   const { trip, userId, showModal, onClick, onDelete } = props;
-  let { trip_id, destination, start_date, end_date, members } = trip;
+  let { trip_id, trip_name, destination, start_date, end_date, members } = trip;
   if (!members) {
     members = [];
   }
+  members = members.filter((member) => !member.isSelf);
+  const memberNames = members.map((member) => member.name ? member.name.split(' ')[0] : "");
 
   function decodePromisified() {
     return new Promise(function(resolve, reject) {
@@ -17,13 +19,39 @@ const Trip = props => {
     })
   }
 
+  function formatDateRange(startDate, endDate) {
+    const endOptions = {
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric'
+    };
+    const startOptions = Object.assign({}, endOptions);
+    startDate = new Date(startDate);
+    endDate = new Date(endDate);
+    if (startDate.getFullYear() === endDate.getFullYear()) {
+      delete startOptions.year;
+    }
+    return startDate.toLocaleDateString('default', startOptions) + " - "
+        + endDate.toLocaleDateString('default', endOptions);
+  }
+
   return (
     <div className="trip row">
       <div className="trip-element trip-img" onClick={onClick}>
         <img src={`../../../../assets/trips/${destination.toLowerCase()}.png`} alt={destination}/>
       </div>
-      <div className="trip-element trip-dest" onClick={onClick}>
-        <span>{destination}</span>
+      <div className="trip-element trip-details" onClick={onClick}>
+        <span className="trip-name">{trip_name}</span>
+        <span className="trip-dest">{destination}</span>
+        <span className="trip-dates">{formatDateRange(start_date, end_date)}</span>
+        <div className="trip-members">
+          <div className="photos">
+            {/* TODO:*/}
+          </div>
+          <div className="names">
+            {[memberNames.slice(0, -1).join(', '), memberNames.slice(-1)[0]].join(memberNames.length < 2 ? '' : ' & ')}
+          </div>
+        </div>
       </div>
       <div className="trip-element delete-button" onClick={onDelete}>
         <span></span>
